c2e078c36f4b4b3e232297c76277c63b16042dd7
Ronnapon <52712562+Ronnapon@users.noreply.github.com>
2021-06-20 20:00:21 +0700

    Add files via upload

---
+++ 10_Array/10_Array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _10_Array
+{
+    class Program
+    {
+
+        // C# Array name of method
+        static void Main(string[] args)
+        {
+            var number = new[] { 1, 2, 3, 4, 5, 6 };
+
+            // Length
+            Console.WriteLine("Length " + number.Length);
+
+            // IndexOf()
+            var indexArray = Array.IndexOf(number, 1);
+            Console.WriteLine(indexArray);
+
+            // Clear()
+            Array.Clear(number, 0, 2);
+
+
+            Console.WriteLine("------------------");
+            foreach(int result in number)
+            {
+                Console.WriteLine(result);
+            }
+
+            // Copy()
+            Console.WriteLine("Copy------------------");
+            var anouther = new int[3] { 5, 6, 1 };
+            Array.Copy(number, anouther, 2);
+            foreach (int result in anouther)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("Sort------------------");
+            // Sort()
+            Array.Sort(number);
+            foreach (int result in number)
+            {
+                Console.WriteLine(result);
+            }
+
+            // Reverse
+            Console.WriteLine("Reverse------------------");
+            Array.Reverse(number);
+            foreach (int result in number)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ 10_list/10_list/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 }); // If See IEnumable canuse array or list
+
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Index of" + numbers.IndexOf(2));
+            Console.WriteLine("LastIndex of" + numbers.LastIndexOf(7));
+            Console.WriteLine("Count "+ numbers.Count);
+
+
+            Console.WriteLine("Remove number 1");
+            for (int i = 0; i < numbers.Count ; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+            }
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Clear>>>>>");
+            numbers.Clear();
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
