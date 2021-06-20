85c72e469967186a738fdc73b08253684313f854
Ronnapon <52712562+Ronnapon@users.noreply.github.com>
2021-06-20 20:08:27 +0700

    Add files via upload

---
+++ 15_procedural_programming/15_procedural_programming/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _15_procedural_programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            var reverse = ReverseName(name);
+            Console.WriteLine(reverse);
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            return new string(array);
+        }
+    }
+}
